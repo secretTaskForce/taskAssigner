afc7fcf1380c35231478c50113e676afe8bf7a1a
David Woolf <davidjohnwoolf@gmail.com>
2015-09-27 16:51:02 -0700

    fix server issue creating empty models

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ function putOne(collname) {
 function postOne(collname) {
 	app.post('/'+collname, function (req, res) {
 		console.log('Receiving new model...');
-		var newid = collname.length;
+		var newid = database[collname].length;
 		console.log('Assigning id of %s',newid);
 		var obj = req.body;
 		obj.id = newid;
